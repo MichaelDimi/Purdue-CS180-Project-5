637cf904ecd40eb97d637663c263b49a485ee642
Aaron Ni <niaar2004@gmail.com>
2022-11-05 17:19:44 -0400

    added customer cart and checkout helper methods

--- src/Customer.java
+++ src/Customer.java
@@ -7,4 +7,64 @@ public class Customer extends User {
     public Customer(String email, String password) {
         super(email, password);
     }
+
+    public HashMap<Book, Integer> getCart() {
+        return cart;
+    }
+
+    public HashMap<Book, Integer> getPurchaseHistory() {
+        return purchaseHistory;
+    }
+
+    // TODO: do error handling and catch negative inputs
+    // adds new books to user's cart
+    public void addToCart(Book book, int quantity) {
+        // current quantity of specified book
+        Integer currentCount = cart.get(book);
+
+        // checks if user already has book in cart, increments current quantity if so
+        if (currentCount == null) { // could be replaced with merge, not sure if Vocareum will like?
+            cart.put(book, quantity);
+        } else {
+            cart.put(book, currentCount + quantity);
+        }
+    }
+
+    // removes books from user's cart
+    public void removeFromCart(Book book, int quantity) throws BookNotFoundException {
+        // current quantity of specified book
+        Integer currentCount = cart.get(book);
+
+        // checks if user already has book in cart, decrements current quantity if so
+        if (currentCount == null) { // could be replaced with merge, not sure if Vocareum will like?
+            throw new BookNotFoundException("Book does not exist in the cart");
+        } else {
+            // removes book from hashmap if final quantity is less than or equal 0
+            if (currentCount - quantity <= 0) {
+                cart.remove(book);
+            } else {
+                cart.put(book, currentCount - quantity);
+            }
+        }
+    }
+
+    public void checkoutCart() {
+        for (HashMap.Entry<Book, Integer> entry : cart.entrySet()) {
+            Integer currentCount = cart.get(entry.getKey());
+
+            if (currentCount == null) { // could be replaced with merge, not sure if Vocareum will like?
+                cart.put(entry.getKey(), entry.getValue());
+            } else {
+                cart.put(entry.getKey(), currentCount + entry.getValue());
+            }
+        }
+    }
+
+    @Override
+    public String toString() {
+        return "Customer{" +
+                "cart=" + cart +
+                ", purchaseHistory=" + purchaseHistory +
+                '}';
+    }
 }

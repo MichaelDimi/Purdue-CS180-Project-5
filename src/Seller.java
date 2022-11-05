40578c83592f369eabf2269d3baa82cf32d3b805
Aaron Ni <niaar2004@gmail.com>
2022-11-05 16:09:20 -0400

    updated user subclasses

--- src/Seller.java
+++ src/Seller.java
@@ -1,5 +1,8 @@
+import java.util.ArrayList;
+
 public class Seller extends User{
-    
+    private ArrayList<Store> stores = new ArrayList<>();
+
     public Seller(String email, String password) {
         super(email, password);
     }

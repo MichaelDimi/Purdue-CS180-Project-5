dcf341887d111f7d2e02b67ded1e49fbebb123f9
SanyaMehra <57568730+SanyaMehra@users.noreply.github.com>
2022-11-05 16:29:27 -0400

    Added Marketplace

--- src/Marketplace.java
+++ src/Marketplace.java
@@ -4,11 +4,11 @@ import java.util.HashMap;
  * 
  */
 public class Marketplace {
-    User[] users = new User[];
-    HashMap<String, Integer> books = new HashMap<>();
+    User[] users = new User[]{}; //TODO: Intialize array dimensions
+    HashMap<Book, Integer> books = new HashMap<>();
     User currentUser = null;
 
-    public String toString() {
+    public String toString() { //TODO: Create toString
         return "";
     }
 

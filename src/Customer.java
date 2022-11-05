16762f2ac2168d8fa4c4b987e6cec64a67f94746
Aaron Ni <niaar2004@gmail.com>
2022-11-05 16:23:30 -0400

    fixed book hashmap order

--- src/Customer.java
+++ src/Customer.java
@@ -1,8 +1,8 @@
 import java.util.HashMap;
 
 public class Customer extends User {
-    private HashMap<Integer, Book> cart = new HashMap<Integer, Book>();
-    private HashMap<Integer, Book> purchaseHistory = new HashMap<Integer, Book>();
+    private HashMap<Book, Integer> cart = new HashMap<Book, Integer>();
+    private HashMap<Book, Integer> purchaseHistory = new HashMap<Book, Integer>();
 
     public Customer(String email, String password) {
         super(email, password);

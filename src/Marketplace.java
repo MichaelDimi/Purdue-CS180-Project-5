58db413043bd8a9c4e79abcc414a6cc656b59bf2
SanyaMehra <57568730+SanyaMehra@users.noreply.github.com>
2022-11-05 16:06:54 -0400

    Added Marketplace

---
+++ src/Marketplace.java
@@ -0,0 +1,15 @@
+import java.util.Arrays;
+import java.util.HashMap;
+/**
+ * 
+ */
+public class Marketplace {
+    User[] users = new User[];
+    HashMap<String, Integer> books = new HashMap<>();
+    User currentUser = null;
+
+    public String toString() {
+        return "";
+    }
+
+}

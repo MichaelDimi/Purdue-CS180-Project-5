ee6bc80ec19877dae8b6fc0c3369e7113cd41e22
Aaron Ni <niaar2004@gmail.com>
2022-11-06 01:59:54 -0500

    updated seller methods

--- src/Seller.java
+++ src/Seller.java
@@ -6,4 +6,19 @@ public class Seller extends User{
     public Seller(String email, String password) {
         super(email, password);
     }
+
+    public void createNewStore(String storeName) {
+        stores.add(new Store(storeName));
+    }
+
+    public ArrayList<Store> getStores() {
+        return stores;
+    }
+
+    @Override
+    public String toString() {
+        return "Seller{" +
+                "stores=" + stores +
+                '}';
+    }
 }

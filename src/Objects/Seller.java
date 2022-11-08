265936b66e928b36b45d08f08af3ca514a707ed5
Aaron Ni <niaar2004@gmail.com>
2022-11-08 16:42:49 -0500

    added ability to for Seller to see and list all books in a specified store

--- src/Objects/Seller.java
+++ src/Objects/Seller.java
@@ -125,63 +125,77 @@ public class Seller extends User implements Serializable {
 
     public void editStoreInventory(Store store) {
         // TODO: ADD AND REMOVE BOOK LISTINGS
-        HashMap<Book, Integer> stock = store.getStock();
 
-        System.out.println("Would you like to:");
-        System.out.println("1. Add new books");
-        System.out.println("2. Remove books");
-        System.out.println("3. Edit existing books");
-        System.out.println("4. View books in store");
-        System.out.println("5. DONE");
+        boolean isEditingInventory = true;
+        while (isEditingInventory) {
+            HashMap<Book, Integer> stock = store.getStock();
 
-        Scanner scanner = new Scanner(System.in);
+            System.out.println("Would you like to:");
+            System.out.println("1. Add new books");
+            System.out.println("2. Remove books");
+            System.out.println("3. Edit existing books");
+            System.out.println("4. View books in store");
+            System.out.println("5. DONE");
 
-        switch (scanner.nextLine()) {
-            case "1":
-                // add books
-                System.out.println("Enter a book name:");
-                String bookName = scanner.nextLine();
+            Scanner scanner = new Scanner(System.in);
 
-                System.out.println("Enter book genre(s):");
-                String genre = scanner.nextLine();
+            switch (scanner.nextLine()) {
+                case "1":
+                    // add books
+                    System.out.println("Enter a book name:");
+                    String bookName = scanner.nextLine();
 
-                System.out.println("Enter book description:");
-                String description = scanner.nextLine();
+                    System.out.println("Enter book genre(s):");
+                    String genre = scanner.nextLine();
 
-                System.out.println("Enter book genre(s):");
-                double price = scanner.nextDouble();
-                scanner.nextLine();
+                    System.out.println("Enter book description:");
+                    String description = scanner.nextLine();
 
-                System.out.println("Enter quantity:");
-                int quantity = scanner.nextInt();
-                scanner.nextLine();
+                    System.out.println("Enter book genre(s):");
+                    double price = scanner.nextDouble();
+                    scanner.nextLine();
 
-                Book book = new Book(bookName, store.getName(), genre, description, price);
+                    System.out.println("Enter quantity:");
+                    int quantity = scanner.nextInt();
+                    scanner.nextLine();
 
-                // loops and adds the quantity of books specified to store's stock
-                for (int i = 0; i < quantity; i++) {
-                    // current quantity of specified book
-                    Integer currentCount = store.getStock().get(book);
+                    Book newBook = new Book(bookName, store.getName(), genre, description, price);
 
-                    // checks if user already has book in cart, increments current quantity if so
-                    if (currentCount == null) { // could be replaced with merge, not sure if Vocareum will like?
-                        stock.put(book, quantity);
-                    } else {
-                        stock.put(book, currentCount + quantity);
+                    // loops and adds the quantity of books specified to store's stock
+                    for (int i = 0; i < quantity; i++) {
+                        // current quantity of specified book
+                        Integer currentCount = stock.get(newBook);
+
+                        // checks if user already has book in cart, increments current quantity if so
+                        if (currentCount == null) { // could be replaced with merge, not sure if Vocareum will like?
+                            stock.put(newBook, quantity);
+                        } else {
+                            stock.put(newBook, currentCount + quantity);
+                        }
                     }
-                }
-                break;
-            case "2":
-                // remove books
-                break;
-            case "3":
-                // edit books
-                break;
-            case "4":
-                // displays all books
-                break;
-            default:
-                break;
+                    break;
+                case "2":
+                    // remove books
+                    break;
+                case "3":
+                    // edit books
+                    break;
+                case "4":
+                    // displays all books
+                    System.out.println("CURRENT STOCK");
+                    System.out.println("*******************");
+
+                    for (Book book : stock.keySet()) {
+                        System.out.println("Name: " + book.getName());
+                        System.out.println("Genre: " + book.getGenre());
+                        System.out.println("Description: " + book.getDescription());
+                        System.out.printf("Price: $%.2f", book.getPrice());
+                        System.out.println("Quantity: " + stock.get(book));
+                    }
+                    break;
+                default:
+                    isEditingInventory = false;
+            }
         }
     }
 

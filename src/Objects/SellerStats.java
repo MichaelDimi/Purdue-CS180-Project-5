d91909f7311216f9859e9927b3fedabbc7daf7e7
Aaron Ni <niaar2004@gmail.com>
2022-11-11 17:49:09 -0500

    added new stats and metrics for Seller

--- src/Objects/SellerStats.java
+++ src/Objects/SellerStats.java
@@ -1,7 +1,6 @@
 package Objects;
 
 import java.io.Serializable;
-import java.util.ArrayList;
 import java.util.HashMap;
 
 public class SellerStats implements Serializable {
@@ -29,6 +28,75 @@ public class SellerStats implements Serializable {
         this.booksSold = new HashMap<>();
         this.revenue = 0;
         this.buyers = new HashMap<>();
+    }
+
+    // formats and prints all books Seller has sold
+    public void listAllSoldBooks() {
+        if (booksSold.size() == 0) {
+            System.out.println("YOU CURRENTLY HAVE NO SOLD BOOKS");
+        } else {
+            // displays all books
+            System.out.println("YOUR SOLD BOOKS");
+            System.out.println("*******************");
+
+            for (Book book : booksSold.keySet()) {
+                System.out.println("Name: " + book.getName());
+                System.out.println("Genre: " + book.getGenre());
+                System.out.println("Description: " + book.getDescription());
+                System.out.printf("Price: $%.2f\n", book.getPrice());
+                System.out.println("Quantity: " + booksSold.get(book) + "\n");
+            }
+        }
+    }
+
+    // formats and prints all the customers of Seller
+    public void listAllBuyers() {
+        if (booksSold.size() == 0) {
+            System.out.println("YOU CURRENTLY HAVE NO SOLD BOOKS");
+        } else {
+            // displays all books
+            System.out.println("YOUR CUSTOMERS");
+            System.out.println("*******************");
+
+            for (User buyer : buyers.keySet()) {
+                System.out.println(buyer.getName());
+                // TODO: get the number of products this user has bought from specific seller
+            }
+        }
+    }
+
+    // prints the most popular genre the Seller sells
+    public void listMostPopularGenre() {
+        if (booksSold.size() == 0) {
+            System.out.println("YOU CURRENTLY HAVE NO SOLD BOOKS");
+        } else {
+            // holds all book genres that have been sold
+            HashMap<String, Integer> genres = new HashMap<>();
+
+            // generates hasmap based on genres
+            for (Book book : booksSold.keySet()) {
+                String bookGenre = book.getGenre();
+                // current quantity of specified book
+                Integer newBookCount = genres.get(bookGenre);
+
+                // checks if user already has book in cart, increments current quantity if so
+                if (newBookCount == null) { // could be replaced with merge, not sure if Vocareum will like?
+                    genres.put(bookGenre, 1);
+                } else {
+                    genres.put(bookGenre, newBookCount + 1);
+                }
+            }
+
+            // gets most popular genre
+            String mostPopular = "";
+            for (String genre : genres.keySet()) {
+                if (genres.get(genre) > genres.get(mostPopular)) {
+                    mostPopular = genre;
+                }
+            }
+
+            System.out.println("YOUR MOST POPULAR GENRE iS: " + mostPopular);
+        }
     }
 
     public HashMap<Book, Integer> getBooksSold() {

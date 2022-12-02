6c86f90b574becc9822cba2d4103703304b7b070
Diyasingh03 <singh959@purdue.edu>
2022-12-02 16:10:27 -0500

    create GUI with login class

---
+++ src/GUI/Login.java
@@ -0,0 +1,19 @@
+package GUI;
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+
+public class Login implements Runnable{
+
+    ActionListener actionListener = new ActionListener() {
+        @Override
+        public void actionPerformed(ActionEvent e) {
+
+        }
+    };
+
+    @Override
+    public void run() {
+
+    }
+}

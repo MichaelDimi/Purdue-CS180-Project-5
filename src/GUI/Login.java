41e046c8f6aaa08fc5eb0da9237cb7dd34419d62
SanyaMehra <57568730+SanyaMehra@users.noreply.github.com>
2022-12-02 16:22:08 -0500

    Added Login basics

--- src/GUI/Login.java
+++ src/GUI/Login.java
@@ -1,19 +1,45 @@
-package GUI;
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
 
-public class Login implements Runnable{
+public class Login extends JFrame implements Runnable {
 
     ActionListener actionListener = new ActionListener() {
         @Override
+        
         public void actionPerformed(ActionEvent e) {
-
+            JOptionPane.showMessageDialog(null, "Login Successful");
         }
     };
 
     @Override
     public void run() {
+        JPanel panel = new JPanel();
+        JFrame frame = new JFrame();
+        Container content = frame.getContentPane();
+        frame.setSize(400, 200);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setVisible(true);
+        
 
+        content.setLayout(new BorderLayout());
+
+        JLabel uLabel = new JLabel("Username");
+        panel.add(uLabel);
+        JTextField uText = new JTextField(15);
+        panel.add(uText);
+        JLabel pLabel = new JLabel("Password");
+        panel.add(pLabel);
+        JTextField pText = new JTextField(15);
+        panel.add(pText);
+        JButton loginButton = new JButton("Log in");
+        panel.add(loginButton);
+
+        content.add(panel, BorderLayout.NORTH);
+        
+    }
+
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(new Login());
     }
 }

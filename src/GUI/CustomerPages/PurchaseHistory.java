6d4ae4d19592988bb6effc5516066af9b99146f6
SanyaMehra <57568730+SanyaMehra@users.noreply.github.com>
2022-12-06 21:58:18 -0500

    Changed Customer to use buttons and added SearchBook

--- src/GUI/CustomerPages/PurchaseHistory.java
+++ src/GUI/CustomerPages/PurchaseHistory.java
@@ -7,18 +7,55 @@ import java.awt.event.ActionListener;
 
 public class PurchaseHistory extends JFrame implements Runnable {
     JPanel panel;
+    JPanel panel2;
+    JPanel panel3;
     JFrame frame;
     Container content;
+    JLabel search;
+    JLabel enter;
+    JTextField searchText;
+    JTextArea note;
+    JLabel note2;
+    JPanel optionPanel;
+
     ActionListener actionListener = new ActionListener() {
         @Override
 
         public void actionPerformed(ActionEvent e) {
         }
     };
+
     public void run() {
 
         panel = new JPanel();
+        optionPanel = new JPanel(new GridLayout(4, 4));
         frame = new JFrame();
-        content = frame.getContentPane();
+        Container content = frame.getContentPane();
+
+        frame.setLocationRelativeTo(null);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        content.setLayout(new BorderLayout(content,BoxLayout.Y_AXIS));
+        search = new JLabel("Your Purchase History");
+        panel.add(search);
+
+        panel2 = new JPanel();
+        enter = new JLabel("Would you like to export your purchase history? (Y/N)");
+        panel2.add(enter);
+
+        searchText = new JTextField(5);
+        panel2.add(searchText);
+
+        content.add(panel, BorderLayout.NORTH);
+        content.add(panel2, BorderLayout.CENTER);
+
+        frame.pack();
+        frame.setSize(400, 400);
+        frame.setVisible(true);
+
+    }
+
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(new PurchaseHistory());
     }
 }
--- src/GUI/CustomerPages/SearchBook.java
+++ src/GUI/CustomerPages/SearchBook.java
@@ -7,18 +7,57 @@ import java.awt.event.ActionListener;
 
 public class SearchBook extends JFrame implements Runnable {
     JPanel panel;
+    JPanel panel2;
+    JPanel panel3;
     JFrame frame;
     Container content;
+    JLabel search;
+    JLabel enter;
+    JTextField searchText;
+    JTextArea note;
+    JLabel note2;
+
     ActionListener actionListener = new ActionListener() {
         @Override
 
         public void actionPerformed(ActionEvent e) {
         }
     };
+
     public void run() {
 
         panel = new JPanel();
         frame = new JFrame();
         content = frame.getContentPane();
+
+        content.setLayout(new BorderLayout());
+        search = new JLabel("Search For A Book");
+        panel.add(search);
+
+        panel2 = new JPanel();
+        enter = new JLabel("Enter a search query:");
+        panel2.add(enter);
+
+        searchText = new JTextField(15);
+        searchText.setAlignmentX(Component.CENTER_ALIGNMENT);
+        panel2.add(searchText);
+
+        panel3 = new JPanel();
+        note = new JTextArea("Note: The query will be used to search for \n name, genre, and description.");
+        note.setEditable(false);
+        panel3.add(note);
+
+        content.add(panel, BorderLayout.NORTH);
+        content.add(panel2, BorderLayout.CENTER);
+        content.add(panel3, BorderLayout.SOUTH);
+
+        frame.pack();
+        frame.setSize(400, 400);
+        frame.setVisible(true);
+
+    }
+
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(new SearchBook());
     }
 }

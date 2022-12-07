4db5b45f7b9926cb0c1019dcb73dc7ba76e3ccbd
SanyaMehra <57568730+SanyaMehra@users.noreply.github.com>
2022-12-07 18:22:20 -0500

    Changed design of SearchBook

--- src/GUI/CustomerPages/SearchBook.java
+++ src/GUI/CustomerPages/SearchBook.java
@@ -1,60 +1,87 @@
 package GUI.CustomerPages;
 
+import Client.ClientQuery;
+import Objects.Book;
+import Objects.Marketplace;
+import Query.Query;
+
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.HashMap;
 
 public class SearchBook extends JFrame implements Runnable {
     JPanel panel;
-    JPanel panel2;
-    JPanel panel3;
     JFrame frame;
     Container content;
-    JLabel search;
-    JLabel enter;
-    JTextField searchText;
-    JTextArea note;
-    JLabel note2;
+    JTextField bookSelection;
+    HashMap<Book, Integer> books;
+    JTextArea listOfBooks;
+    Query booksQuery;
+    JButton selectBook;
 
     ActionListener actionListener = new ActionListener() {
         @Override
 
         public void actionPerformed(ActionEvent e) {
+            Book[] booksArr = new Book[books.size()];
+            booksArr = books.keySet().toArray(booksArr);
+            
+            if (e.getSource() == selectBook) {
+                System.out.println(bookSelection.getText());
+            }
         }
     };
-
     public void run() {
+        booksQuery = new ClientQuery().getQuery(null, "books", "*");
+        //TODO: display message when there's a server error
+        // if (booksQuery.getObject().equals(false)) {
+        //     JOptionPane.showMessageDialog(null, "Whoops: There was an error getting the books from the server");
+        // }
+        books = (HashMap<Book, Integer>) booksQuery.getObject();
+
+        // Convert the hashmap to an array, since its easier to manipulate
+        Book[] booksArr = new Book[books.size()];
+        booksArr = books.keySet().toArray(booksArr);
+
+        frame = new JFrame("Search For A Book");
+        content = frame.getContentPane();
+        frame.setLocationRelativeTo(null);
+        frame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+        content.setLayout(new BorderLayout());
+
+        //TODO: implement selecting a book
+        panel = new JPanel();
+        JLabel select = new JLabel("Select a book to buy (enter number):");
+        panel.add(select);
+        bookSelection = new JTextField(10);
+        panel.add(bookSelection);
+        selectBook = new JButton("Select");
+        selectBook.addActionListener(actionListener);
+        panel.add(selectBook);
+        frame.add(panel, BorderLayout.NORTH);
 
         panel = new JPanel();
-        frame = new JFrame();
-        content = frame.getContentPane();
+        int i = 1;
+        StringBuilder bookList = new StringBuilder();
+        for (Book book : booksArr) { //Printing list of books available for sale
 
-        content.setLayout(new BorderLayout());
-        search = new JLabel("Search For A Book");
-        panel.add(search);
+            bookList.append(String.format("%d. %s -- Store: %s -- Original Price: $%.2f " +
+                            "-- Percent off: %.2f%% -- Final price: $%.2f -- " +
+                            "Quantity: " + "[%d]\n",
+                    i, book.getName(), book.getStore(), book.getPrice(), book.getPercentOff(), book.finalPrice(), books.get(book)));
+            i++;
+        }
+        listOfBooks = new JTextArea(bookList.toString());
+        listOfBooks.setEditable(false);
+        panel.add(listOfBooks);
+        content.add(panel, BorderLayout.CENTER);
 
-        panel2 = new JPanel();
-        enter = new JLabel("Enter a search query:");
-        panel2.add(enter);
-
-        searchText = new JTextField(15);
-        searchText.setAlignmentX(Component.CENTER_ALIGNMENT);
-        panel2.add(searchText);
-
-        panel3 = new JPanel();
-        note = new JTextArea("Note: The query will be used to search for \n name, genre, and description.");
-        note.setEditable(false);
-        panel3.add(note);
-
-        content.add(panel, BorderLayout.NORTH);
-        content.add(panel2, BorderLayout.CENTER);
-        content.add(panel3, BorderLayout.SOUTH);
-
+        
         frame.pack();
-        frame.setSize(400, 400);
+        //frame.setSize();
         frame.setVisible(true);
-
     }
 
     public static void main(String[] args) {

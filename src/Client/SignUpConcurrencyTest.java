50603a29060147c229268c1691c9e89643ba8389
Aaron Ni <niaar2004@gmail.com>
2022-12-11 17:06:34 -0500

    updated concurrency test

--- src/Client/SignUpConcurrencyTest.java
+++ src/Client/SignUpConcurrencyTest.java
@@ -8,33 +8,29 @@ public class SignUpConcurrencyTest extends Menu {
         String username;
         String email;
         String password;
-        boolean validationSuccess;
-        do {
-            username = input[0];
-            email = input[1];
-            password = input[2];
 
-            boolean isBuyer = true;
+        username = input[0];
+        email = input[1];
+        password = input[2];
 
-            System.out.println("Validating...");
+        boolean isBuyer = true;
 
-            // Add user
-            Query validateAddQuery = new ClientQuery().updateQuery(null, "users", "add", new String[]{username, email, password, isBuyer ? "T" : "F"});
-            if (validateAddQuery.getObject().equals(false)) {
-                if (validateAddQuery.getOptions().equals("validation err")) {
-                    System.out.println("Whoops: Validation failed. Please try again");
-                } else if (validateAddQuery.getOptions().equals("hash err")) {
-                    System.out.println("Whoops: Unable to hash password");
-                } else {
-                    System.out.println("Whoops: Unable to create your account. Please try again");
-                }
-                validationSuccess = false;
+        System.out.println("Validating...");
+
+        // Add user
+        Query validateAddQuery = new ClientQuery().updateQuery(null, "users", "add", new String[]{username, email, password, isBuyer ? "T" : "F"});
+        if (validateAddQuery.getObject().equals(false)) {
+            if (validateAddQuery.getOptions().equals("validation err")) {
+                System.out.println("Whoops: Validation failed. Please try again");
+            } else if (validateAddQuery.getOptions().equals("hash err")) {
+                System.out.println("Whoops: Unable to hash password");
             } else {
-                System.out.println("Validation successful!");
-                BookApp.currentUser = new User("", "", "", ""); //TODO: CHEck THis
-                validationSuccess = true;
+                System.out.println("Whoops: Unable to create your account. Please try again");
             }
-        } while (!validationSuccess);
+        } else {
+            System.out.println("Validation successful!");
+            BookApp.currentUser = new User("", "", "", ""); //TODO: CHEck THis
+        }
 
         return false;
     }
